74501485dbdbb53707c59eadab5fa3291c46b9c6
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-03-29 22:05:28 +0530

    feat: Navbar section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Sidebar from "./Components/Sidebar";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
     <div className="flex">
       <Sidebar />
+      <div className="grow ml-16 md:ml-64 h-full lg:h-screen bg-gray-100 text-gray-900">
+        <Navbar />
+      </div>
     </div>
   );
 };
